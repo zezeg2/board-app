9350e9be5eb2d89e006e3f5228fd91d5495dae4d
jonghyeon <whdgus003@gmail.com>
2022-08-26 17:30:14 +0900

    Custom Validation Pipe

---
+++ src/boards/pipe/board-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../boards.model";
+
+export class BoardStatusValidationPipe implements PipeTransform{
+
+    readonly StatusOptions = [
+        BoardStatus.PUBLIC,
+        BoardStatus.PRIVATE,
+
+    ]
+    transform(value: any, metadata: ArgumentMetadata): any {
+
+        console.log('metadata', metadata);
+        value = value.toUpperCase();
+        if (!this.isStatusValid(value)) throw new BadRequestException(`${value} isn't in the status options`);
+        return value;
+    }
+
+    private isStatusValid(value: any) {
+        const index = this.StatusOptions.indexOf(value);
+        return index !== -1
+    }
+}
